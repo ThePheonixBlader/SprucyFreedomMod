b358e672591cabdf73f36a3df1a2271ea8c77c81
ThePheonixBlader <thepheonixblader@gmail.com>
2016-01-17 00:36:02 -0800

    Create Command_admininfo.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -0,0 +1,27 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_CONSOLE, blockHostConsole = true)
+@CommandParameters(
+		description = "Close server to non-superadmins.", 
+		usage = "/<command> [on | off]",
+		aliases = "ai")
+public class Command_admininfo extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+    	sender.sendMessage(ChatColor.GOLD + "So, you want to apply for admin?");
+    	sender.sendMessage(ChatColor.RED + "Well head on over to sprucyfreedom.boards.net and register!");
+    	sender.sendMessage(ChatColor.RED + "Make sure you meet the requirements, first of all!");
+    	sender.sendMessage(ChatColor.AQUA + "Then apply!");
+    	sender.sendMessage(ChatColor.GOLD + "Good Luck, " + sender.getName());
+
+    	return true;
+}
+}
