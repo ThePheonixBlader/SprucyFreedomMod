b77c2453aaed7cb2f25a08b37c0d103b03fa7cbf
ThePheonixBlader <thepheonixblader@gmail.com>
2016-01-18 12:56:37 -0800

    Update TFM_Util.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -67,7 +67,7 @@ public class TFM_Util
     private static final Map<String, Integer> ejectTracker = new HashMap<String, Integer>();
     public static final Map<String, EntityType> mobtypes = new HashMap<String, EntityType>();
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
-    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK", "TaahThePhoenix");
+    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
     public static final List<String> EXECUTIVES = Arrays.asList();
     public static final List<String> SYSADMINCMDS = Arrays.asList();
     public static final List<String> SYSTEMADMINS =  Arrays.asList();
